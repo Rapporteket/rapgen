e4749912d1d0dc1fddff8e9713a9cb7d7dcdb801
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-09-28 10:23:55 +0200

    initial commit

---
+++ R/rapgen.R
@@ -0,0 +1,18 @@
+#' rapgen: Multi-registry reports and results at Rapporteket
+#'
+#'  An R package for functions and stuff that neither fits into 'rapbase' nor
+#'  any registry spesiffic package. Initially set up for reports and results
+#'  spanning across registries at Rapporteket
+#'
+#' @section rapgen functions:
+#' \code{\link{RegDelay}}
+#' \code{\link{RegDelayData}}
+#' \code{\link{RegDelayDataPeculiar}}
+#' \code{\link{RegDelayReport}}
+#'
+#' @section rapgen reports:
+#' RegDelayReport.Rmd
+#'
+#' @docType package
+#' @name rapgen
+NULL
